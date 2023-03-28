eb233b43b8b289a14fc7343e12cbb73547588cb1
Swati <swatic019@gmail.com>
2023-03-28 18:17:30 +0530

    firebase updated

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  Timestamp,
+} from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js";
+import {
+  query,
+  orderBy,
+  limit,
+  where,
+  onSnapshot,
+} from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBNpq_CywhebNsEEMg8cxjNfIHRBQw-ygk",
   authDomain: "mytwitterclone-43c08.firebaseapp.com",
-  databaseURL:
-    "https://mytwitterclone-43c08-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL: "https://mytwitterclone-43c08-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "mytwitterclone-43c08",
   storageBucket: "mytwitterclone-43c08.appspot.com",
   messagingSenderId: "178203321423",
-  appId: "1:178203321423:web:9e35de5b8e51458098f63a",
+  appId: "1:178203321423:web:9e35de5b8e51458098f63a"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-import {
-  getDatabase,
-  set,
-  get,
-  update,
-  remove,
-  ref,
-  child,
-} from "https://www.gstatic.com/firebasejs/9.18.0/firebase-database.js";
-
-const db = getDatabase();
+const db = getFirestore(app);
 
 let postTweetInput = document.getElementById("postTweetInput");
 let addTweet = document.getElementById("addTweet");
 let tweetsContainer = document.getElementById("tweetsContainer");
 
 function addATweet() {
-  set(ref(db, "Tweets/" + postTweetInput.value), {
-    tweet: postTweetInput.value,
+  addDoc(collection(db, "tweets"), {
+    content: postTweetInput.value,
   })
     .then(() => {
       alert("Data added sucessfully!");
@@ -49,28 +49,25 @@ function addATweet() {
 }
 
 function postATweet() {
-  const dbref = ref(db);
-
-  get(child(dbref, "Tweets/" + postTweetInput.value))
-    .then((snapshot) => {
-      if (snapshot.exists()) {
-        let tweetHTML = `
+  let tweetHTML = `
         <div class="tweet">
         <div class="profileImgTweet"></div>
         <div class="tweetContent">
-            <h3>Swati Thorat</h3>
-            <p>${snapshot.val().tweet}</p>
+        <h3>Swati Thorat</h3>
+        <p>${postTweetInput.value}</p>
         </div>
         </div> `;
-        tweetsContainer.insertAdjacentHTML("beforeend", tweetHTML);
-      } else {
-        alert("Please add a tweet");
-      }
-    })
-    .catch((error) => {
-      alert(error);
-    });
+  tweetsContainer.insertAdjacentHTML("beforeend", tweetHTML);
   postTweetInput.value = "";
 }
 
 addTweet.addEventListener("click", addATweet);
+
+// async function fetchAllTweets() {
+//   const dbref = ref(db);
+//   let allTweets = await get(child(dbref, "Tweets/"));
+//   console.log(allTweets.val());
+
+// }
+
+// fetchAllTweets();
